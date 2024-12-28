150257c3753a85f200fc83f52416b09401c16ae8
Vikraam17 <vikyvino1773@gmail.com>
2024-12-28 15:40:12 +0530

    hoisting

---
+++ Javascript/Hoistihng.js
@@ -0,0 +1,14 @@
+console.log(a);
+var a=10; // global
+console.log(a);  // undefined 10
+
+// console.log(a);
+// let a=10; 
+// console.log(a); // Error
+
+// console.log(a);
+// const a=10;
+// console.log(a); // Error
+
+// Cannot access 'a' before initialization
+// let is temporary
