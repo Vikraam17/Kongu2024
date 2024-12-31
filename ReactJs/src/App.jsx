e4aa75b402e144bad4d1aa4ce07e3cc2d80578a4
Vikraam17 <vikyvino1773@gmail.com>
2024-12-31 09:25:52 +0530

    State Hook added

--- ReactJs/src/App.jsx
+++ ReactJs/src/App.jsx
@@ -2,33 +2,10 @@ import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
 import { Routes, Route, BrowserRouter as Router, Link } from "react-router-dom";
+import State from "./components/StateHook";
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ol>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/AboutUs">About Us</Link>
-            </li>
-            <li>
-              <Link to="/ContactUs">Contact Us</Link>
-            </li>
-          </ol>
-        </nav>
-      </div>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/AboutUs" element={<AboutUs />} />
-        <Route path="/ContactUs" element={<ContactUs />} />
-      </Routes>
-    </Router>
-  );
+  return <State />;
 }
 
 export default App;
