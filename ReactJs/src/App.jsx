62ed873ba38f3bf72cdbdfa000ec45f614983b61
Vikraam17 <vikyvino1773@gmail.com>
2024-12-31 10:12:28 +0530

    Effect,Ref Hook added

--- ReactJs/src/App.jsx
+++ ReactJs/src/App.jsx
@@ -2,10 +2,35 @@ import Home from "./components/Home";
 import AboutUs from "./components/AboutUs";
 import ContactUs from "./components/ContactUs";
 import { Routes, Route, BrowserRouter as Router, Link } from "react-router-dom";
-import State from "./components/StateHook";
+import StateHook from "./components/StateHook";
 
 function App() {
-  return <State />;
+  return (
+    <StateHook/>
+    // <Router>
+    //   <div>
+    //     <nav>
+    //       <ol>
+    //         <li>
+    //           <Link to="/">Home</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/AboutUs">About Us</Link>
+    //         </li>
+    //         <li>
+    //           <Link to="/ContactUs">Contact Us</Link>
+    //         </li>
+    //       </ol>
+    //     </nav>
+    //   </div>
+
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/AboutUs" element={<AboutUs />} />
+    //     <Route path="/ContactUs" element={<ContactUs />} />
+    //   </Routes>
+    // </Router>
+  );
 }
 
 export default App;
--- ReactJs/src/components/StateHook.jsx
+++ ReactJs/src/components/StateHook.jsx
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 const StateHook = () => {
   const [count, setCount] = useState(0);
+  let countRef = useRef(0);
+
+  let increment = () => {
+    setCount(count + 1);
+    countRef.current++;
+    // console.log("Count : ", count);
+    console.log("Ref : ",countRef.current);
+  };
+
+  // useEffect(() => {
+  //   console.log("useEffect Invoked");
+  //   return console.log("Value updated");
+  // },[count]);
+
   return (
     <div>
-      <h1>Count : {count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <h1>Count : {countRef.current}</h1>
+      <button onClick={increment}>Increment</button>
+      {/* <button onClick={() => setCount(count - 1)}>Decrement</button> */}
     </div>
   );
 };
