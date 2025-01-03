5acc91f50c01de05b4b7eb22074fed8a6502eab7
Vikraam17 <vikyvino1773@gmail.com>
2025-01-03 09:56:52 +0530

    Mongoose is added

--- KECBackEnd/index.js
+++ KECBackEnd/index.js
@@ -1,17 +1,25 @@
 var express = require("express");
 var app = express();
 const PORT = 3001;
+
 var path = require('path');
 
+var mdb = require('mongoose');
+mdb.connect("mongodb://localhost:27017").then(()=>{
+  console.log("MongoDB Connection successful");
+}).catch((err)=>{
+  console.log("Check your Connection String");
+  console.log(err);
+})
 
 app.get('/',(req,res)=>{
   res.send("Welcome to Backend Server");
 })
 
 app.get('/json',(req,res)=>{
-  
   res.json({server: "Welcome to Backend Server", url:"http://localhost:3001",port:3000});
 })
+
 app.get('/static',(req,res)=>{
   console.log(__dirname);
   res.sendFile(path.join(__dirname,"index.html"));
