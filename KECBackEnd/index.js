314da97b2d3925f758447690535eebec9058f82c
Vikraam17 <vikyvino1773@gmail.com>
2025-01-03 09:39:53 +0530

    Path set Up

--- KECBackEnd/index.js
+++ KECBackEnd/index.js
@@ -1,17 +1,20 @@
 var express = require("express");
 var app = express();
 const PORT = 3001;
+var path = require('path');
+
+
 app.get('/',(req,res)=>{
   res.send("Welcome to Backend Server");
 })
 
-app.get('/JSON',(req,res)=>{
+app.get('/json',(req,res)=>{
   
   res.json({server: "Welcome to Backend Server", url:"http://localhost:3001",port:3000});
 })
 app.get('/static',(req,res)=>{
-  
-  res.sendFile('D:/MERN/Kongu/KECBackEnd/index.html')
+  console.log(__dirname);
+  res.sendFile(path.join(__dirname,"index.html"));
 })
 
 
