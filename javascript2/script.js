94798d8a9d5e6e3ba02a1cea68deb52bb9e2d20e
Vikraam17 <vikyvino1773@gmail.com>
2025-01-24 20:52:13 +0530

    js added

---
+++ javascript2/script.js
@@ -0,0 +1,18 @@
+var array={
+  name:"viky",
+  sname:"v",
+  "phn no":"9876543210"
+}
+console.log(array)
+
+for(let key in array){
+  console.log(key)
+}
+var array=[1,2,3,4,5]
+for(let value of array)
+{
+  console.log(value)
+}
+for(let i=0;i<array.length;i++){
+  console.log(array[i]);
+}
--- loginPage/script.js
+++ loginPage/script.js
@@ -1,1 +1,20 @@
-const username=document.getElementById('user').value;
+function handleLogin() {
+  const username = document.getElementById("user").value;
+  const password = document.getElementById("pass").value;
+  const error = document.getElementById("error");
+
+  const LoginData = [
+    { username: "viky", password: "123" },
+    { username: "dheepu", password: "123" },
+  ];
+
+  const isValid = LoginData.some(
+    (user) => user.username === username && user.password === password
+  );
+  if (isValid) {
+    window.location.href = "../index.html";
+    errorMsg.innerText = "Login Successfull";
+  } else {
+    errorMsg.innerText = "Invalid Login";
+  }
+}
