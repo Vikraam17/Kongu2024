f3256ace3c04b4a26c130cabf9f86676e85fc5ad
Vikraam17 <vikyvino1773@gmail.com>
2025-01-27 16:51:46 +0530

    Game added

--- loginPage/script.js
+++ loginPage/script.js
@@ -2,7 +2,6 @@ function handleLogin() {
   const username = document.getElementById("user").value;
   const password = document.getElementById("pass").value;
   const error = document.getElementById("error");
-
   const LoginData = [
     { username: "viky", password: "123" },
     { username: "dheepu", password: "123" },
@@ -11,10 +10,11 @@ function handleLogin() {
   const isValid = LoginData.some(
     (user) => user.username === username && user.password === password
   );
+
   if (isValid) {
     window.location.href = "../index.html";
-    errorMsg.innerText = "Login Successfull";
+    error.innerText = "Login Successfull";
   } else {
-    errorMsg.innerText = "Invalid Login";
+    error.innerText = "Invalid Login";
   }
 }
